c6adcf7c2389ca6e59bb04456a32c51bc87cdf7d
imatyaj1221212 <newsafwan@outlook.com>
2024-07-30 16:05:56 +0530

    imatyaj.sol

--- fx-portal-main/contracts/imatyaj.sol
+++ fx-portal-main/contracts/imatyaj.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.9;
 
 import "erc721a/contracts/ERC721A.sol";
 
-contract nomme is ERC721A {
+contract imatyaj is ERC721A {
     address public owner;
 
     uint256 public maxQuantity = 5;
 
     
     string baseUrl =
-        "https://indigo-immense-barnacle-223.mypinata.cloud/ipfs/QmYAguVHDGpTpHTP1bpG9VmVhZHJt5RHQii4TvAxbCWUqo/";
+        your prefer url;
 
 
-    string public prompt = "not me";
-    constructor() ERC721A("CuteCats", "CC") {
+    string public prompt = "Imatryaj";
+    constructor() ERC721A("ImatYaj", "IM") {
         owner = msg.sender;
     }
 
